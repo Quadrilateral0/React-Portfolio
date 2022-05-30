8f1fa20afb3947aeb51d3f070732d5b40cc01c8b
Quadrilateral <amandacdulaney@gmail.com>
2022-05-30 16:48:19 -0400

    added contact section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
+import Contact from './components/Contact';
 
 export default function App() {
   return (
@@ -21,6 +22,10 @@ export default function App() {
 
     <section  id="resume" class="sectionbox">
     <Resume />
+    </section>
+
+    <section  id="contact" class="sectionbox">
+    <Contact />
     </section>
 
     </div>
