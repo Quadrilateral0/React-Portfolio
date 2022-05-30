e5ec2bbf73cf5ca7bcd471eadbe332b54b313935
Quadrilateral <amandacdulaney@gmail.com>
2022-05-30 15:32:52 -0400

    added src with header and about sections

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import About from './components/About';
+
+export default function App() {
+  return (
+    <div>
+    <header id="home" class="header" aria-label="A peachy mountain view.">
+        <h1>
+            AMANDA&nbsp;&nbsp;C&nbsp;&nbsp;DULANEY
+        </h1>
+        <nav aria-label="header navigation">
+            <ul>
+                <li><a href="#about">About Me</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#resume">Resum&eacute;</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section id="about" class="sectionbox">
+    <About />
+    </section>
+
+    <section  id="projects" class="sectionbox">
+    <About />
+    </section>
+
+    </div>
+  );
+}
