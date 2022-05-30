3732c3dd72587dfb5905dc276c16e99a59aa653d
Quadrilateral <amandacdulaney@gmail.com>
2022-05-30 16:52:46 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
+import Footer from  './components/Footer';
 
 export default function App() {
   return (
     <div>
-    <section id="header" class="sectionbox">
+
+    <section id="home" class="header" aria-label="A peachy mountain view.">
     <Header />
     </section>
 
@@ -26,6 +28,10 @@ export default function App() {
 
     <section  id="contact" class="sectionbox">
     <Contact />
+    </section>
+
+    <section class="footer">
+    <Footer />
     </section>
 
     </div>
