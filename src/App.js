432a0d1c3ab9ae835d89465e1d500c28631e7e40
Quadrilateral <amandacdulaney@gmail.com>
2022-05-30 16:35:52 -0400

    reorg header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
@@ -6,19 +7,9 @@ import Resume from './components/Resume';
 export default function App() {
   return (
     <div>
-    <header id="home" class="header" aria-label="A peachy mountain view.">
-        <h1>
-            AMANDA&nbsp;&nbsp;C&nbsp;&nbsp;DULANEY
-        </h1>
-        <nav aria-label="header navigation">
-            <ul>
-                <li><a href="#about">About Me</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#resume">Resum&eacute;</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
+    <section id="header" class="sectionbox">
+    <Header />
+    </section>
 
     <section id="about" class="sectionbox">
     <About />
